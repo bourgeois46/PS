d9c04618aa19295ab9b3c195ca51bd566fe46529
Bourgeois <133302526+bourgeois46@users.noreply.github.com>
2023-05-14 17:01:40 +0900

    Create bubbleSort.c

---
+++ week7/bubbleSort.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void bubbleSort(int* A, int n) {
+	int i, j, temp;
+	/*i = 0 n-1 회 버블
+	* i = 1 n-2 회 버블
+	* i = 2 n-3 회 버블
+	*/
+	for (i = 0; i < n-1; i--) 
+		for (j = 0; j <= n-i-1; j++)
+			if (A[j] > A[j + 1]) {//큰 것과 자기 자신 자리 바꿈
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+
+}
+int main() {
+	int n, i;
+	int* A;
+
+	scanf("%d", &n);
+
+	A = (int*)malloc(sizeof(int) * n);
+
+	if (n = NULL)
+		return 0;
+
+	for (i = 0; i < n; i++) {//정렬 전
+		A[i] = rand() % 1000;
+		printf("%d ", A[i]);
+	}
+
+	bubbleSort(A, n);
+
+	printf("\n");
+	for (i = 0; i < n; i++) {//정렬 후
+		printf("%d ", A[i]);
+	}
+
+	free(A);
+}
