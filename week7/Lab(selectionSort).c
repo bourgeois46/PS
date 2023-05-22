41ca95c55872c58fb6a17da68c8dfacd4f23e133
Bourgeois <133302526+bourgeois46@users.noreply.github.com>
2023-05-22 11:44:53 +0900

    Update Lab(selectionSort).c

--- week7/Lab(selectionSort).c
+++ week7/Lab(selectionSort).c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/* 각 루프마다 최대 원소 찾고, 최대 원소와 맨 오른쪽 원소를 교환한다. 처음에 첫번째 원소를 max로 설정한다. 범위에서 맨 오른쪽 원소를 제외한다.
+이를 원소가 하나만 남을 때까지 반복 -> n-1 회*/
+
 void selectionSort(int *A, int n) {
 	int i, j, temp;
 
@@ -9,8 +13,8 @@ void selectionSort(int *A, int n) {
 		int max_idx = 0;
 		//max 제외
 		for (j = 1; j < n - i; j++) 
-			//max 2번째 값부터 비교할 거라서 j = 1 부터
-			if (A[max_idx] < A[j]) 
+			//max 2번째 값부터 비교,  j = 1 부터
+			if (A[max_idx] < A[j])//내림차순
 				max_idx = j;
 
 		//A[max_id] <-> A[n-1-i]/swap
@@ -19,6 +23,7 @@ void selectionSort(int *A, int n) {
 		A[n - 1 - i] = temp;
 	}
 }
+
 int main() {
 	int n, i;
 	int* A;
@@ -27,12 +32,12 @@ int main() {
 
 	scanf("%d", &n);
 
-	if (input_number <= 0)
+	if (n <= 0)
 		return 0; 
 
 	A = (int*)malloc(sizeof(int) * n);
 
-	if (p = NULL)
+	if (A = NULL)
 		return 0;
 
 	for (i = 0; i < n; i++) {//정렬 전
